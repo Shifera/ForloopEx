408569d735cc2736464d04b323cf789c5a4c308c
Shifera <shiferawregassa@gmail.com>
2019-10-23 17:13:14 -0400

    first push

---
+++ src/ForloopEx.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ForloopEx {
+
+    public static void main(String[] args) {
+int x;
+
+for(x=1; x<=9;x=x+1){
+    System.out.println(x+1);
+}
+
+//        // QUIZ: print number from 1 to USER INPUT, and also from 1 squared to USER INPUT squared.
+//        Scanner keyboard = new Scanner(System.in);
+//        int userInput;
+//        int square;
+//
+//        // ask user how many times to run our for loop
+//        System.out.print("Enter any positive number?");
+//        userInput = keyboard.nextInt();
+//
+//        // run your for loop, using the number the user gave as the UPPER LIMIT
+//        for (int loopCounter = 1; loopCounter <= userInput; loopCounter = loopCounter + 1) {
+//
+//            // we are going to print ONE LINE AT A TIME...
+//            // the loop will do it FOR US!!!....
+//            // suppose user said run loop eight times
+//            // it shouldn't matter to our logic, which number time we're on...
+//
+//            square = loopCounter * loopCounter;   // store the square value temporarily
+//            System.out.println("the loopCounter is " + loopCounter + " the square of it is " + square);
+//
+//        } // end for of QUIZ
+
+    }  // end main
+}
